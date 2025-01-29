f51a2d64a96e2f3abee8829c0c41da4745969dc8
Gabriel P. <148332067+ModdedGearV@users.noreply.github.com>
2025-01-29 15:30:59 +0100

    servo

---
+++ spaceGroupe/paul/fenêtre_servo.py
@@ -0,0 +1,24 @@
+from machine import Pin, PWM
+from time import sleep
+
+servo = PWM(Pin(26), freq=50)
+
+def porteOuverte():
+    """
+    Ouvre la porte en réglant le servo sur une position spécifique.
+    Cette fonction ajuste le rapport cyclique (duty cycle) du servo à 70,
+    ce qui correspond à l'ouverture de la fenêtre. Elle attend ensuite 1 seconde
+    pour permettre au servo de se stabiliser.
+    """
+    servo.duty(70)
+    sleep(1)
+    
+def porteFermee():
+    """
+    Ferme la porte en réglant le servo sur une position spécifique.
+    Cette fonction ajuste le rapport cyclique (duty cycle) du servo à 20,
+    ce qui correspond à la fermeture de la fenêtre. Elle attend ensuite 1 seconde
+    pour permettre au servo de se stabiliser.
+    """
+    servo.duty(20)
+    sleep(1)
